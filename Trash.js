0ef1e0bfb06deec0b26e3702cfc4b2c31e8ed4be
125AritraBasakJoy <basakjoy125@gmail.com>
2025-08-19 20:56:42 +0600

    Add Trash.js

--- Trash.js
+++ Trash.js
@@ -1,27 +1,48 @@
-switch(new Date().getDay()) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-        break;
-    default:
-        day = "Unknown";
-        break;
-    }
-    console.log("Today is: "+ new Date().getDay());
+
+let userName = "Malfoy";
+let secretCode = "12345";
+if (secretCode === "12345") {
+    console.log("Welcome, " + userName + "!");
+}
+else {
+    console.log("Access denied.");
+}
+
+let myString = "Harry"; 
+let age = 20;
+let checkValidation = (age => 18) ? "Access Entry to Hogwarts" : "Access Denied";
+console.log(checkValidation);
+myString += " Potter";
+console.log(myString);
+
+const information = {
+    name: "Hermione",
+    age: 18,
+    house: "Gryffindor"
+};
+delete information.age;
+console.log(information);
+
+const seriesName = ["Game of Thrones", "Breaking Bad", "Stranger Things", "The Witcher"];
+seriesName.push("The Crown");
+console.log(seriesName);
+const movieName = ["Harry Potter", "The Goblet of Fire", "The Deathly Hallows", "The Intern", "The Dark Knight"];
+const movieReleaseYear = [2001, 2005, 2010, 2015, 2018];
+console.log(" ");
+console.log(movieName.at(1));
+console.log(" ");
+movieName.splice(2, 0, "The Philosopher's Stone");
+console.log(movieName);
+movieName.splice(3, 1);
+console.log(movieName);
+let movieAndSeries = movieName.concat(seriesName);
+console.log(" ");
+console.log("Movie and Series List: " + movieAndSeries.join(", "));
+console.log(" ");
+// console.log("Movie List: "+ movieName.join(", ").concat(" and Series List: " + seriesName.join(", ")));
+const carName = ["Toyota", "Honda", "BMW", "Mercedes"];
+const iterator = carName.entries();
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+//const check = movieReleaseYear.every(year => year > 2000);
