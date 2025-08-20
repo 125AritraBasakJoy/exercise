8b6acafaf58613f52922bea8ddf227c8e6f17cf7
125AritraBasakJoy <basakjoy125@gmail.com>
2025-08-20 18:00:02 +0600

    Add ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -1,4 +1,12 @@
-function reverseString(str){
-    return str.split('').reverse().join('');
-}
-console.log(reverseString("joy"));
+// function reverseString(str){
+//     return str.split('').reverse().join('');
+// }
+// console.log(reverseString("joy"));
+
+const myArray = "joy is my name ".split(" ");
+function reverseString(myArray) {
+    return myArray.reduceRight(function (accumulator, currentValue) {
+        return accumulator + " " + currentValue;
+    }, "");
+}   
+console.log(reverseString(myArray)); 
