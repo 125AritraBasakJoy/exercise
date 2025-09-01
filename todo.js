7361e32fabba2ad5ecffcd259c5db1a00fc2da29
Aritra Basak Joy <basakjoy125@gmail.com>
2025-09-01 22:19:33 +0600

    Add todo.js

--- todo.js
+++ todo.js
@@ -1,29 +1,44 @@
 const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
-const errorMsg = document.querySelector("small"); 
+const errorMsg = document.querySelector("small");
+const descriptionInput = document.getElementById("textdesc"); 
 
 addTaskButton.addEventListener("click", () => {
     const taskText = taskInput.value.trim();
-    if(taskText === ""){
-        errorMsg.style.visibility="visible";
+    const desc = descriptionInput.value.trim();
+
+    if (taskText === "") {
+        errorMsg.style.visibility = "visible";
         return;
     }
 
-    errorMsg.style.visibility="hidden";
+    errorMsg.style.visibility = "hidden";
 
     const list = document.createElement("li");
-    list.textContent = taskText;
+
+
+    const taskTitleEl = document.createElement("strong");
+    taskTitleEl.textContent = taskText;
+    list.appendChild(taskTitleEl);
+
+    if (desc !== "") {
+        const taskDescEl = document.createElement("p");
+        taskDescEl.textContent = desc;
+        list.appendChild(taskDescEl);
+    }
 
     const delBtn = document.createElement("button");
     delBtn.textContent = "Delete";
     delBtn.onclick = () => list.remove();
-    list.appendChild(delBtn);
     delBtn.style.backgroundColor = "#7C4585";
     delBtn.style.color = "#fff";
     delBtn.style.borderRadius = "5px";
+    delBtn.style.marginLeft = "10px";
+    list.appendChild(delBtn);
 
-    taskList.appendChild(list);
+    taskList.prepend(list);
 
     taskInput.value = "";
+    descriptionInput.value = "";
 });
